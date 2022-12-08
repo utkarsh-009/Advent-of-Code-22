f587949675d175e0a4c6a26ee9965a7f58298deb
utkarsh-009 <utkarshpatil321@gmail.com>
2022-12-08 19:19:47 +0530

    Done

---
+++ day7p1.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define mod 1000000007
+using namespace std;
+
+void solve()
+{
+#ifndef ONLINE_JUDGE
+    freopen("day7INPUT.txt", "r", stdin);
+    // freopen("output.txt", "w", stdin);
+#endif
+
+    unordered_map<int, vector<> lvl;
+    unordered_map<string, unordered_map<string, int>> fileSys;
+    for (int l = 0; l < 1045;)
+    {
+        string s;
+        getline(cin, s);
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+}
+
+/*
+ASCII VALUES
+    0-9 => 48-57
+    A-Z => 65-90
+    a-z => 97-122
+*/
